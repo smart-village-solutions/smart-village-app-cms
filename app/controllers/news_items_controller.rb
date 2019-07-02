338ca7bdc5f05c00e2969fe9a60fbb507db8b0d1
Sebastian Peter <sebastian.peter@ikusei.de>
2019-07-02 14:30:16 +0200

    Implement funcionality for news_item controller
    * new Action: add Open Struct for has one/has many submodels of  news_item. Necessary to ensure the form behaves the way it would with an ActiveRecord Model
    * edit Action: Add query to retrieve news_item to be edited from graphql server and save it in global variable, which is used in edit form
    * update Action: Copy logic from EventRecord#update. Implement a Flash Error message if a validation of a new record on the main app server failed and reload the news_item to be edited.
    * Add converter method which converts nested attributes for double has_many association to make graphql conversion and building of newsItem mutation work

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -23,17 +23,157 @@ class NewsItemsController < ApplicationController
   end
 
   def new
+    @news_item = OpenStruct.new(
+      address: OpenStruct.new(),
+      source_url: OpenStruct.new,
+      media_contents: [OpenStruct.new(source_url: OpenStruct.new)]
+    )
   end
 
   def edit
+    results = @smart_village.query <<~GRAPHQL
+    query {
+      newsItem(id: #{params[:id]}) {
+        id
+        author
+        fullVersion
+        charactersToBeShown
+        publicationDate
+        publishedAt
+        title
+        externalId
+        sourceUrl {
+          url
+          description
+        }
+        address {
+          addition
+          street
+          zip
+          city
+          geoLocation {
+            latitude
+            longitude
+          }
+        }
+        dataProvider {
+          name
+          contact {
+            firstName
+            lastName
+            phone
+            fax
+            email
+            webUrls {
+              url
+              description
+            }
+          }
+          address {
+            addition
+            street
+            zip
+            city
+            geoLocation {
+              latitude
+              longitude
+            }
+          }
+          logo {
+            url
+            description
+          }
+        }
+        contentBlocks {
+          title
+          intro
+          body
+          mediaContents {
+            id
+            captionText
+            contentType
+            copyright
+            height
+            width
+            sourceUrl {
+              url
+              description
+            }
+          }
+        }
+	    }
+    }
+    GRAPHQL
+
+    @news_item = results.data.news_item
   end
 
   def create
   end
 
   def update
+    old_id = params[:id]
+    logger.warn("*"*100)
+    query = create_params
+    logger.warn(query)
+
+    begin
+      results = @smart_village.query query
+      byebug
+    rescue Graphlient::Errors::GraphQLError => e
+      flash[:error] = e.errors.messages["data"].to_s
+      redirect_to edit_news_item_path(old_id)
+      return
+    end
+
+    new_id = results.data.create_news_item.id
+    # ToDo Check if destroy_record was succesfull
+    if new_id.present? && new_id != old_id
+      # Nach dem Erstellen des neuen Datensatzes wird der alte gelöscht
+
+      destroy_results = @smart_village.query <<~GRAPHQL
+        mutation {
+          destroyRecord(id: #{old_id}, recordType: "NewsItem") {
+            id
+            status
+            statusCode
+          }
+        }
+      GRAPHQL
+
+      redirect_to edit_news_item_path(new_id)
+    else
+      flash[:error] = "Fehler: #{results.errors.inspect}"
+      redirect_to edit_news_item_path(old_id)
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def create_params
+    @news_item_params = params.require(:news_item).permit!
+    convert_params_for_graphql
+    Converter::Base.new.build_mutation('createNewsItem', @news_item_params)
+  end
+
+  def convert_params_for_graphql
+      # Convert has_many content_blocks(which has_many media_contents)
+      content_block_params = @news_item_params["content_blocks"]
+      return unless content_block_params.present?
+
+      content_blocks = []
+      media_contents = []
+      content_block_params.each do |key, content_block|
+        next if content_block.blank?
+        content_block[:media_contents].each do |key, media_content|
+          media_contents << media_content
+        end
+        content_block[:media_contents] = media_contents
+        content_blocks << content_block
+      end
+      @news_item_params["content_blocks"] = content_blocks
   end
 end
