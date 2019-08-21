8bd5a18681bd6f89041861bee48d0230493c541b
Marco Metz <m.metz@ikusei.de>
2019-08-21 10:45:39 +0200

    Absicherung Wenn kein User vorhanden ist
    Falls kein current_user vorhanden ist soll kein Fehler geworfen werden

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
   def visible_in_role?(role_name)
+    return false if @current_user.blank?
+    return false if @current_user.roles.blank?
+
     @current_user.roles.fetch(role_name, false) == true
   end
 end
