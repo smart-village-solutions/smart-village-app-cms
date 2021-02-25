e0e82aad9604d98f322d2ba1f21733bbb9c635d8
Daniel Molnar <dm@tpwd.de>
2021-02-25 14:11:55 +0100

    fix(waste calendar): handle empty data in create process
    SVA-130

--- app/controllers/waste_calendar_controller.rb
+++ app/controllers/waste_calendar_controller.rb
@@ -48,23 +48,26 @@ class WasteCalendarController < ApplicationController
     parsed_address_data = CSV.parse(@address_data, headers: true, col_sep: col_separator_address)
     parsed_tour_data = CSV.parse(@tour_data, headers: true, col_sep: col_separator_tour)
 
+    redirect_to action: :new and return unless parsed_address_data.present? && parsed_tour_data.present?
+
     @address_data_selects = parsed_address_data.first.headers
     @tour_data_selects = parsed_tour_data.first.headers
 
-    if params[:address].present? && params[:tour].present?
-      Thread.new do
-        waste_importer = Importer::WasteCalendar.new(
-          smart_village: @smart_village,
-          waste_types: @waste_types,
-          address_data: parsed_address_data,
-          address_assignment: params[:address],
-          tour_data: parsed_tour_data,
-          tour_assignment: params[:tour]
-        )
-        waste_importer.perform
-      end
-      flash[:notice] = "Import im Hintergrund gestartet ... "
-      redirect_to action: :index
+    return unless params[:address].present? && params[:tour].present?
+
+    Thread.new do
+      waste_importer = Importer::WasteCalendar.new(
+        smart_village: @smart_village,
+        waste_types: @waste_types,
+        address_data: parsed_address_data,
+        address_assignment: params[:address],
+        tour_data: parsed_tour_data,
+        tour_assignment: params[:tour]
+      )
+      waste_importer.perform
     end
+
+    flash[:notice] = "Import im Hintergrund gestartet ..."
+    redirect_to action: :index
   end
 end
