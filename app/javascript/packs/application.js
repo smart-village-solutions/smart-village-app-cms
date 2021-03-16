677901f684256c916a31ec9738fff7aecef7a08f
Theresa Steinmetz <ts@tpwd.de>
2021-03-16 14:13:56 +0100

    fix(sidebar): change sidebar collapse logic
    - Error was displayed, that .collapse was not a function
    - Therefore just used toggle class logic
    - When smaller than 768 sidebar is collapsed per default
    - Body class wasn't used and therefore got deleted
    
    SVA-181

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -75,17 +75,13 @@ $(function() {
 
   // Toggle the side navigation
   $('#sidebarToggle, #sidebarToggleTop').on('click', function(e) {
-    $('body').toggleClass('sidebar-toggled');
     $('.sidebar').toggleClass('toggled');
-    if ($('.sidebar').hasClass('toggled')) {
-      $('.sidebar .collapse').collapse('hide');
-    }
   });
 
   // Close any open menu accordions when window is resized below 768px
   $(window).resize(function() {
     if ($(window).width() < 768) {
-      $('.sidebar .collapse').collapse('hide');
+      $('.sidebar').addClass('toggled');
     }
   });
 
