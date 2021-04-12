2e06764ba6eeeb3ad2d11c36e4d8e43c02f9e26e
Theresa Steinmetz <ts@tpwd.de>
2021-04-12 16:20:19 +0200

    feat(dashboard): show info about elements
    - add queries for all elements to controller
    - display every category with its amount of elements
    and link to index and new
    
    SVA-206

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,117 @@
 
 class DashboardController < ApplicationController
   before_action :verify_current_user
+  before_action :init_graphql_client
 
   def index
+    job_results = @smart_village.query <<~GRAPHQL
+      query {
+        genericItems(genericType: "Job") {
+          id
+          title
+          payload
+          dataProvider {
+            name
+          }
+          updatedAt
+          createdAt
+        }
+      }
+    GRAPHQL
+
+    news_results = @smart_village.query <<~GRAPHQL
+      query {
+        newsItems {
+          id
+          title
+          dataProvider {
+            name
+          }
+          visible
+          contentBlocks {
+            title
+          }
+          updatedAt
+          createdAt
+          pushNotificationsSentAt
+        }
+      }
+    GRAPHQL
+
+    event_results = @smart_village.query <<~GRAPHQL
+      query {
+        eventRecords {
+          id
+          title
+          visible
+          dataProvider {
+            name
+          }
+          dates {
+            dateStart
+            dateEnd
+          }
+          updatedAt
+          createdAt
+        }
+      }
+    GRAPHQL
+
+    offer_results = @smart_village.query <<~GRAPHQL
+      query {
+        genericItems(genericType: "Offer") {
+          id
+          title
+          dataProvider {
+            name
+          }
+          updatedAt
+          createdAt
+        }
+      }
+    GRAPHQL
+
+    poi_results = @smart_village.query <<~GRAPHQL
+      query {
+        pointsOfInterest {
+          id
+          name
+          visible
+          dataProvider{
+            name
+          }
+          updatedAt
+          createdAt
+        }
+      }
+    GRAPHQL
+
+    tour_results = @smart_village.query <<~GRAPHQL
+      query {
+        tours {
+          id
+          name
+          visible
+          dataProvider {
+            name
+          }
+          updatedAt
+          createdAt
+        }
+      }
+    GRAPHQL
+
+    @jobs = job_results.data.generic_items
+
+    @news_items = news_results.data.news_items
+
+    @events = event_results.data.event_records
+
+    @offers = offer_results.data.generic_items
+
+    @points_of_interest = poi_results.data.points_of_interest
+
+    @tours = tour_results.data.tours
   end
 
 end
