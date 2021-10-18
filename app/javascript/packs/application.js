bd01722db090bf94ccc3a3b044c525a741d3e39e
Theresa Steinmetz <ts@tpwd.de>
2021-10-18 17:05:35 +0200

    feat(map): render leaflet map
    - updated integration of leaflet per node modules
    - fixed rendering of multiple address blocks with geolocation
      - added index to name/id of inputs
      - added has_many params converter for addresses
    - preselected pin on map, if there are geo coordinates existing
      - adjusted zoom level as well to keep the location more close
        if coordinates exist
    - fixed variable name `fo` to `fa` in order to correctly access
      organizer address data for showing geo location data
    
    SVA-227

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,9 @@ require('../partials/_nested_forms');
 require('../partials/_nested_content_block_form');
 require('../partials/_nested_lunch_form');
 
+// map in forms
+require('../partials/_leaflet_map');
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
--- app/javascript/partials/_leaflet_map.js
+++ app/javascript/partials/_leaflet_map.js
@@ -1,40 +1,73 @@
-const initLeafletMap = () => {
-  // Leaflet Integration
-  var map = L.map('leafletMap', {
-    scrollWheelZoom: false,
+var L = require('leaflet/dist/leaflet');
+var LGH = require('leaflet-gesture-handling/dist/leaflet-gesture-handling');
+
+/* eslint-disable func-names */
+var CENTER_OF_GERMANY = [51.3127114, 9.4797461];
+var DEFAULT_ZOOM_LEVEL = 6;
+
+function initLeafletMap(id) {
+  var $parentRow = $('#' + id).parent().parent().parent();
+  var $geoLocationLatitude = $parentRow.find('[id$="geo_location_latitude"]');
+  var $geoLocationLongitude = $parentRow.find('[id$="geo_location_longitude"]');
+
+  var center = CENTER_OF_GERMANY;
+  var zoom = DEFAULT_ZOOM_LEVEL;
+  if ($geoLocationLatitude.val() && $geoLocationLongitude.val()) {
+    // Set center to eventually existing lat lng values
+    center = [$geoLocationLatitude.val(), $geoLocationLongitude.val()];
+    // Set zoom more close to eventually existing lat lng values
+    zoom = 13;
+  }
+
+  L.Map.addInitHook('addHandler', 'gestureHandling', LGH.GestureHandling);
+
+  var map = L.map(id, {
     minZoom: 3,
-  }).setView([ 51.3127114, 9.4797461], 6);
+    zoom,
+    center,
+    gestureHandling: true
+  });
 
   var icon = L.icon({
     iconUrl: '/assets/marker.svg',
-    iconSize:     [30, 30],
-    iconAnchor:   [15, 30]
+    iconSize: [30, 30],
+    iconAnchor: [15, 30]
   });
 
-  var marker = {};
-  marker = L.marker([ 51.3127114, 9.4797461], {icon: icon}).addTo(map);
-  map.on('click',function(e){
-    lat = e.latlng.lat;
-    lon = e.latlng.lng;
+  var marker;
+  if ($geoLocationLatitude.val() && $geoLocationLongitude.val()) {
+    // Set marker for eventually existing lat lng values
+    marker = L.marker(center, { icon: icon }).addTo(map);
+  }
 
-    // Clear existing marker
+  map.on('click', function(e) {
+    var lat = e.latlng.lat;
+    var lng = e.latlng.lng;
+
+    // Clear existing marker before setting a new one
     if (marker != undefined) {
       map.removeLayer(marker);
-    };
+    }
 
-     marker = L.marker([lat,lon], {icon: icon}).addTo(map);
+    // Set the new marker on clicked position
+    marker = L.marker([lat, lng], { icon: icon }).addTo(map);
+
+    $geoLocationLatitude.val(lat);
+    $geoLocationLongitude.val(lng);
   });
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
-};
+}
 
 $(function() {
-  // Check if map exists on page
-  if($('#leafletMap').length) {
-    setTimeout(function () {
-      initLeafletMap();
-    }, 500);
-  }
+  // Check if map exists on page after a short timeout being rendered and init maps in a loop
+  setTimeout(function() {
+    $('.leafletMap').each(function() {
+      initLeafletMap(this.id);
+    });
+  }, 500);
 });
+/* eslint-ensable func-names */
