df30379ab75042d9394beed20497d2cf0aae6d0e
Daniel Molnar <dm@tpwd.de>
2022-11-10 16:22:11 +0100

    feat(push notifications): create with title and body for all devices
    SVA-710

---
+++ app/controllers/push_notifications_controller.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class PushNotificationsController < ApplicationController
+  before_action :verify_current_user
+  before_action { verify_current_user_role("role_push_notification") }
+  before_action :init_graphql_client
+
+  def new
+    @push_notification = new_push_notification
+  end
+
+  def create
+    query = create_params
+    begin
+      results = @smart_village.query query
+    rescue Graphlient::Errors::GraphQLError => e
+      flash[:error] = e.errors.messages["data"].to_s
+      @push_notification = new_push_notification
+      render :new
+      return
+    end
+    flash[:notice] = "Push-Notification wurde erstellt"
+    redirect_to new_push_notification_path
+  end
+
+  private
+
+    def new_push_notification
+      OpenStruct.new(
+        title: "",
+        body: ""
+      )
+    end
+
+    def create_params
+      @push_notification_params = params.require(:push_notification).permit!
+      Converter::Base.new.build_mutation("sendPushNotification", @push_notification_params)
+    end
+end
