757b1b40b84121bae2fa475fd8c7f10ec8f84f4b
Daniel <dm@tpwd.de>
2022-12-08 15:13:54 +0100

    feat(deadlines): store publication date
    - mobile app uses the publication date first in index items
    
    SVA-710

--- app/controllers/deadlines_controller.rb
+++ app/controllers/deadlines_controller.rb
@@ -239,6 +239,7 @@ class DeadlinesController < ApplicationController
           dates << date
         end
         @deadline_params["dates"] = dates
+        @deadline_params["publication_date"] = dates.first.dig(:date_start) if dates.present?
       end
 
       # Take push notification params for scheduling in separate mutation
